7a203914a0ccf535374ba88a80428cd2f755d9a2
Rahul <rahuldavu23@gmail.com>
2025-04-07 01:51:59 -0400

    Just did another function

--- src/arrays.ts
+++ src/arrays.ts
@@ -31,7 +31,10 @@ export function tripleNumbers(numbers: number[]): number[] {
  * the number cannot be parsed as an integer, convert it to 0 instead.
  */
 export function stringsToIntegers(numbers: string[]): number[] {
-    return [];
+    let newNumbers = numbers.map((num: string): number =>
+        Number(num) ? parseInt(num) : 0,
+    );
+    return newNumbers;
 }
 
 /**
