d019c797b08a1f9830daf0b92e703eda2a9dd08e
Rahul <rahuldavu23@gmail.com>
2025-04-14 19:49:28 -0400

    did Remove Dollars

--- src/arrays.ts
+++ src/arrays.ts
@@ -45,7 +45,10 @@ export function stringsToIntegers(numbers: string[]): number[] {
  */
 // Remember, you can write functions as lambdas too! They work exactly the same.
 export const removeDollars = (amounts: string[]): number[] => {
-    return [];
+    let newAmounts = amounts.map((amount: string): number =>
+        Number(amount) ? parseInt(amount) : 0,
+    );
+    return newAmounts;
 };
 
 /**
